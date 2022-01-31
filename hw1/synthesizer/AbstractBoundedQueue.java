bb9f0651d5b928168768b8a3dcffa4add81c4a92
Amo-zwk <1425698748@qq.com>
2022-01-31 23:47:16 +0800

    1

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -15,10 +15,4 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
         return fillCount;
     } //题目要求
 
-    public abstract T peek();
-
-    public abstract T dequeue();
-
-    public abstract void enqueue(T x); //未知
-
 }
