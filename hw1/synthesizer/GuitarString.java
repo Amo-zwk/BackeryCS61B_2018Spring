2e9cac36e11a398082a1e0062b8ea68c5fa93076
Amo-zwk <1425698748@qq.com>
2022-02-01 01:03:46 +0800

    1

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,45 +1,40 @@
 package synthesizer;
 
-//Make sure this class is public
 public class GuitarString {
     /**
-     * Constants. Do not change. In case you're curious, the keyword final means
-     * the values cannot be changed at runtime. We'll discuss this and other topics
-     * in lecture on Friday.
+     * Constants. Do not change. In case you're curious, the keyword final means the
+     * values cannot be changed at runtime. We'll discuss this and other topics in
+     * lecture on Friday.
      */
-    private static final int SR = 44100;      // Sampling Rate
+    private static final int SR = 44100; // Sampling Rate
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
     private BoundedQueue<Double> buffer;
 
-    /* Create a guitar string of the given frequency.  */
+    /* Create a guitar string of the given frequency. */
     public GuitarString(double frequency) {
-        int capacity = (int) (Math.round(SR / frequency));
-        buffer = new ArrayRingBuffer<>(capacity); //这句话是哪里得出信息这么写的
+        buffer = new ArrayRingBuffer<>((int) Math.round(SR / frequency));
         for (int i = 0; i < buffer.capacity(); i++) {
-            buffer.enqueue(0.0); //filled with zeors
+            buffer.enqueue(0.0);
         }
     }
 
-
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        //       Make sure that your random numbers are different from each other.
         for (int i = 0; i < buffer.capacity(); i++) {
             buffer.dequeue();
             buffer.enqueue(Math.random() - 0.5);
         }
     }
 
-    /* Advance the simulation one time step by performing one iteration of
-     * the Karplus-Strong algorithm.
+    /*
+     * Advance the simulation one time step by performing one iteration of the
+     * Karplus-Strong algorithm.
      */
     public void tic() {
-
-        double oldFirst = buffer.dequeue();
-        double newFirst = buffer.peek();
-        buffer.enqueue((oldFirst + newFirst) / 2 * 0.996); //题目图中得到
+        double frontSample = buffer.dequeue();
+        buffer.enqueue(DECAY * 0.5 * (frontSample + sample()));
     }
 
     /* Return the double at the front of the buffer. */
