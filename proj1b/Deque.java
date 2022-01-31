7de131677bf85e5f0cf9e920d644102036e03bc6
Amo-zwk <1425698748@qq.com>
2022-01-31 14:15:11 +0800

    1

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,12 +1,13 @@
 public interface Deque<T> {
     //因为是泛型,所以要在类名后面加<T>
     //接口都是这样的形式，后面是冒号
-    void addFirst(T item);
-    void addLast(T item);
-    T removeFirst();
-    T removeLast();
+    //接口的时候省略public
     boolean isEmpty();
     int size();
+    void addFirst(T item);
+    void addLast(T item);
     void printDeque();
+    T removeFirst();
+    T removeLast();
     T get(int index);
 }
