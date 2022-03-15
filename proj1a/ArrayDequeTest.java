bf57d18befb5a536ff6cf4f2e3dc5931ebc96c04
Amo-zwk <1425698748@qq.com>
2022-03-15 21:34:20 +0800

    1

--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -9,18 +9,18 @@ public class ArrayDequeTest {
     //判空的测试
     @Test
     public void empty() {
-        ArrayDeque<String> dq = new ArrayDeque<>() ;
+        ArrayDeque<String> dq = new ArrayDeque<>();
         assertTrue(dq.isEmpty());
     }
     //判断size和头插
     @Test
     public void size() {
-        ArrayDeque<String> dq = new ArrayDeque<>() ;
+        ArrayDeque<String> dq = new ArrayDeque<>();
         dq.addFirst("first");
-        String first = dq.removeFirst() ;
-        assertEquals("first",first);
+        String first = dq.removeFirst();
+        assertEquals("first", first);
         dq.addFirst("second");
-        assertEquals(1,dq.size());
+        assertEquals(1, dq.size());
     }
     //
 }
