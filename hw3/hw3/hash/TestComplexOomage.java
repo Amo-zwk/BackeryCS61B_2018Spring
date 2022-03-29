f5431efe6f849b5ecef0f5ac25a6e678967add6c
Amo-zwk <1425698748@qq.com>
2022-03-29 17:08:43 +0800

    1

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -38,25 +38,33 @@ public class TestComplexOomage {
     /*
      * that shows the flaw in the hashCode function.
      */
+    private ArrayList<Integer> randomList() {
+        int N = StdRandom.uniform(1, 10);
+        ArrayList<Integer> params = new ArrayList<>(N);
+        for (int i = 0; i < N; i += 1) {
+            params.add(StdRandom.uniform(0, 255));
+        }
+        return params;
+    }
 
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
-        int deadlyListSize = 10000;
-        for (int i = 0; i < deadlyListSize; i++) {
-            int N = StdRandom.uniform(1, 10);
-            ArrayList<Integer> params = new ArrayList<>(N);
-            for (int j = 0; j < N - 4; j += 1) {
-                params.add(StdRandom.uniform(0, 255));
-            }
-            for (int j = N - 4; j < N; j++) {
-                params.add(1);
-            }
+
+        // Your code here.
+        int N = 100;
+        ArrayList<Integer> suffix = new ArrayList<>();
+        suffix.add(1);
+        suffix.add(2);
+        suffix.add(3);
+        suffix.add(4);
+        for (int i = 0; i < N; i++) {
+            ArrayList<Integer> params = randomList();
+            params.addAll(suffix);
             deadlyList.add(new ComplexOomage(params));
         }
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
     }
-
 
     /**
      * Calls tests for SimpleOomage.
@@ -65,3 +73,4 @@ public class TestComplexOomage {
         jh61b.junit.textui.runClasses(TestComplexOomage.class);
     }
 }
+
